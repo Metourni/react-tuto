739ae6052b2512903c7123b9ce65a14c5613526d
Metourni Noureddine <en_metourni@esi.dz>
2020-05-30 18:05:56 +0100

    first commit.

--- src/App.js
+++ src/App.js
@@ -2,22 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function FullnameComponent(props){
+  return (
+    <div>{props.firstName} {props.lastName}</div>
+  )
+}
+
+function MyFirstComp(props){
+  return (
+    <div>Hello <FullnameComponent firstName={props.firstName} lastName={props.lastName} /></div>
+  )
+}
+
+class MySecondComp extends React.Component{
+  render(){
+    return (
+      <div>Salut Mr, {this.props.name}</div>
+    )
+  }
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <MyFirstComp firstName="Meto" lastName="Opop"></MyFirstComp>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          <MyFirstComp firstName="Mohamed" lastName="Amine"></MyFirstComp>
+        </p>
+
+        <p>
+          <MySecondComp name="Amine"></MySecondComp>
+        </p>
       </header>
     </div>
   );
